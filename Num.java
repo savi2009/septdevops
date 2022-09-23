9b34d38c5d019cebfd740cdba30aaa912e61a6ac
savi2009 <savi.ak2009@gmail.com>
2022-09-23 19:56:29 +0530

    Num.java added

---
+++ Num.java
@@ -0,0 +1,8 @@
+public class Num{
+     public static void main (String[]args)
+     { int i;
+       for (i=0; i<=10; i++)
+          { System.out.println("numers is " +i);
+          }
+     }
+   }
