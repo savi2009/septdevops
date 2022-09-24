47703362de87d259ed0d140abfcddfc7e9c2e71d
rdubeyrah <114082278+rdubeyrah@users.noreply.github.com>
2022-09-24 12:15:55 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,17 +2,23 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        byte MyByteValue = 10;
-        short MyShortValue = 20;
-        int MyIntValue = 50;
+        double firstVaribale = 20.00;
+        double secondVariable = 80.00;
 
-        long MyLongValue = 50000L+(10*(MyByteValue+MyShortValue+MyIntValue));
-        System.out.println(MyLongValue);
+        double result= 100.00 * (firstVaribale+secondVariable);// 10000.00
+        System.out.println("addition result is ="+result);
+        double remainder = result%40.00; // 10000%40.00
+        System.out.println("remainder result is ="+remainder);
 
-        short shortTotal = (short) (1000 + 10 * (MyByteValue+MyShortValue+MyIntValue));
+        boolean isNoremainder = (remainder==0)? true:false;
+        System.out.println("isNoremainder="+isNoremainder);
+        if (!isNoremainder){
+            System.out.println("Got some remainder");
+        }
+        boolean isremainder = (remainder!= 0)? true:false;
+        if (!isremainder){
+            System.out.println("I got it");
+        }
 
-
-        ;
     }
-
 }
